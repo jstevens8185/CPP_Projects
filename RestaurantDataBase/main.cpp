ae3dbb34ab3ad64ae4478d6995e221d9db1121c8
Jered Stevens <jstevens8185@gmail.com>
2019-10-22 01:14:20 -0500

    Start of database

---
+++ RestaurantDataBase/main.cpp
@@ -0,0 +1,28 @@
+/*
+*   Saves orders into a list.
+*   Each order has a unique order number.
+*/
+
+/*  OrderList
+*   
+*   List of orders.
+*   This is the list that contains
+*   each individual order(ticket).
+*/
+
+/*  Tickets
+*
+*   List that contains food items, drinks,
+*   Alcohol, and desserts
+*/
+
+/*  Food
+*   
+*   Consists of Sandwiches and plates
+*/
+
+/*  Sandwiches
+*
+*   Contains BBQ sandwich, texan, triple, toaster
+*/
+
