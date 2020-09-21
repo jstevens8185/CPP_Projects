38a460b6ff19693be3da80d1fbe47be808ec34f5
Jered Stevens <j.stevens8185@gmail.com>
2020-09-20 22:50:20 -0500

    commented

--- CodingHelp/JeredStevensA1.cpp
+++ CodingHelp/JeredStevensA1.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+/*******************************************************
+ * struct:      discHolder
+ * 
+ * params:      none or 3 floats
+ * 
+ * 
+ * whats it do: holds discriminates
+ ********************************************************/
+
 struct discHolder{
     float disc1;
     float disc2;
@@ -27,9 +36,21 @@ class Matrix{
     private:
         int array[3][3];
         int a1 , a2 , a3 , b1 , b2 , b3 , c1 , c2 , c3;
+        discHolder discriminates;
         float numerator1, numerator2, numerator3;
 
     public:
+
+/*******************************************************
+ * Function:    myMatrix()
+ * 
+ * params:      none
+ * 
+ * returns:     void
+ * 
+ * whats it do: asks for ints and constructs a 2d array
+ *              out of them
+ ********************************************************/
         void myMatrix(){
 
             for (int r = 0; r < 3; r++){
@@ -52,22 +73,55 @@ class Matrix{
             c3 = array[2][2];
         };
 
+
+/*******************************************************
+ * Function:    valueOfDiscriminate
+ * 
+ * params:      none
+ * 
+ * returns:     discHolder
+ * 
+ * whats it do: calculates the value of the discriminates
+ *              and stores it in the class's "discriminates" variable
+ ********************************************************/
+
         discHolder valueOfDiscriminate(){
 
             float disc1, disc2, disc3;
 
-            disc1 = ((b1^2) - 4 * a1 * c1);
-            disc2 = ((b2^2) - 4 * a2 * c2);
-            disc3 = ((b3^2) - 4 * a3 * c3);
+            discriminates.disc1 = ((b1^2) - 4 * a1 * c1);
+            discriminates.disc2 = ((b2^2) - 4 * a2 * c2);
+            discriminates.disc3 = ((b3^2) - 4 * a3 * c3);
 
-            return discHolder(disc1, disc2, disc3);
+            return discriminates;
         };
 
         void valueOfNumerator(){
             
         }
 
-        void print(discHolder discs){
+        void valueOfDenominator(){
+            
+        }
+
+        void printRoots(){
+
+        }
+
+        void showMeTheMatrix(){
+
+        }
+
+/*******************************************************
+ * Function:    print
+ * 
+ * params:      discHolder
+ * 
+ * returns:     void
+ * 
+ * whats it do: For Testing purposes. prints stuff
+ ********************************************************/
+        void print(/*discHolder discs*/){
 
             //PRINT MATRIX
             // for (int c = 0; c < 3; c++){
